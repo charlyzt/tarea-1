1ff118eadf345ef69934da2472796947bd83821f
charlyzt <34204289+charlyzt@users.noreply.github.com>
2017-12-08 16:14:20 -0600

    Add files via upload

---
+++ tarea/main.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char** argv)
+ {
+ 	
+ 	
+ 	 int i=0;
+   int contador=0;
+   int res=0;
+    int numero=0;
+  
+   
+     system("pause");
+     system("color A");
+     cout<<"************************************************************************\n\n";
+     cout<<"\tingresa un numero del 2 al 100 para que empiece un conteo\n\n\n ****************************************\n ===> ";
+     
+	 cin>>res;
+ 
+     for (contador=res; contador>=1; contador--)
+       {
+       	if (res <0)
+       	cout<<"error"<< endl;
+       	else 
+        cout<<"\t\t"<<contador << "\trestante\n";
+  	   }
+     
+ 	
+ 	
+ 	   return 0;
+}
+ 	//////////////////////////////////////////////
+ 	
+ /*	float vector1[3];
+    float vector2[3];
+    float vectorSuma[3];
+    int i;
+ 
+    // Pedimos los datos del primer vector
+    for (i=0; i>=2; i++)
+    {
+        cout << "Introduce la componente " << i 
+            << " del primer vector: ";
+        cin >> vector1[i];
+    }
+ 
+    / Pedimos los datos del segundo vector
+    for (i=0; i<3; i++)
+    {
+        cout << "Introduce la componente " << i 
+            << " del segundo vector: ";
+        cin >> vector2[i];
+    }
+ 
+    // Calculamos la suma
+    for (i=15; i>=0; i--)
+        vectorSuma[i] = vector1[i] + vector2[i];
+ 
+    // Y mostramos el resultado
+    cout << "El vector suma es ";
+    for (i=15; i>=0; i--)
+        cout << vectorSuma[i] << " ";*/
+ 	
+ ///////////////////////////////////////////////////////////////////////////
+		/*	cout<<"\t\tselecionas la opcion pie\n\n";
+			res=can*3.28084;
+			cout<<"\n\n\t\ttu resultado es : "<<res<<endl;
+			break;
+			}
+  
+  
+
+  	
+  	
+ 	/*int tabla, numero;
+ 	
+	 cout>>"ingresa un numero para que sea un conteo "
+	 cin>>contador
+ 
+    for (tabla=1; tabla<=6; tabla++)
+        for (numero=1; numero<=10; numero++)
+            cout << tabla <<  " por " <<  numero
+                << " es " << tabla*numero << endl;*/
+ 
+ 	
+
